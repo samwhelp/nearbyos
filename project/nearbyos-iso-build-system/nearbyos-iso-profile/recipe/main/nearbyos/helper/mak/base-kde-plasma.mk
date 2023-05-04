a4377e994512bee4915a1f11076c1685ddd72ac0
samwhelp <samwhelp@users.noreply.github.com>
2023-05-04 21:32:30 +0800

    fix

---
+++ project/nearbyos-iso-build-system/nearbyos-iso-profile/recipe/main/nearbyos/helper/mak/base-kde-plasma.mk
@@ -0,0 +1,5 @@
+
+
+base-kde-plasma:
+	$(THE_DISTRO_DIR_PATH)/base-kde-plasma/bin/build.sh
+.PHONY: base-kde-plasma
---
+++ project/nearbyos-iso-build-system/nearbyos-iso-profile/recipe/main/nearbyos/helper/mak/main-kde-plasma.mk
@@ -0,0 +1,5 @@
+
+
+main-kde-plasma:
+	$(THE_DISTRO_DIR_PATH)/main-kde-plasma/bin/build.sh
+.PHONY: main-kde-plasma
